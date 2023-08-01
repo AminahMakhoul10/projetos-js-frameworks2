708d5866166b7d36e3adf41fb600326801938555
aminah <aminahmakhoul@gmail.com>
2023-08-01 19:31:19 -0400

    Clonando]

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css"
+export default function Rodape(){
+    return(
+        <>
+        <footer className={styles.rodape}>
+            Rodape do App transporte
+        </footer>
+        </>
+    )
+}
